33860601df0d8abd3ecca548c7a2fba871ce7b9e
constanzaramos <c.ramosgamboa@gmail.com>
2022-01-26 10:46:23 -0300

    json,componentes menu

---
+++ src/Components/Breakfast.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Data from 
+export const Breakfast = () =>{
+    const 
+    return(
+        <section className="breakfast-container">
+            <div>
+                const 
+            </div>
+
+        </section>
+
+    )
+}
+export default Breakfast;
--- src/Components/Waiters.jsx
+++ src/Components/Waiters.jsx
@@ -1,10 +1,32 @@
-import React from "react";
+import React from "react"
+import "../css/meseros.css"
  
 const Waiters = () => {
     return (
-        <section className="waiters">
+        <main className="waiters">
+            <div>
             <h1>PEDIDOS</h1>
-        </section>
+            <button className="home-icon" ><i class="fas fa-home"></i></button>
+            <button className="menu">Comandas listas</button>
+            </div>
+            <div>
+            <input className="input" type="text" placeholder="Nombre del cliente" />
+            <input className="input" type="text" placeholder="Número de mesa" />
+            <button className="menu">OK</button>
+            </div>
+            <section className="containerMenu">
+                <button className="menu">Desayunos</button>
+                <button className="menu">Hamburguesas</button>
+                <button className="menu">Acompañamientos</button>
+                <button className="menu">Para tomar</button>
+            </section>
+            <section>
+                <div className="menu-data"></div>
+                <div className="resumen">
+                <button className="menu">Enviar pedidos</button>
+                </div>
+            </section>
+        </main>
     )
 }
 export default Waiters;
